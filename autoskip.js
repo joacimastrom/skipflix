4c4b2b5fc8c10408d268cf9552953289649adf66
Joacim Åström <joacim.astrom@gmail.com>
2018-11-23 10:04:52 +0100

    Trigger click on play after skip

--- autoskip.js
+++ autoskip.js
@@ -1,18 +1,27 @@
-
 // Send each DOM mutation through a filtering function.
 const obs = new MutationObserver(mutations => mutations.map(matchAddedNodes));
 
 // Send each added node through the matching and clicking function.
 matchAddedNodes = mutation => mutation.addedNodes.forEach(matchAndClick);
 
-// Try matching a node for the "skip" button and click on positive match.
-matchAndClick = node => {
-  if (node.nodeType === 1 && node.matches('.skip-credits'))
-    click(node)
-}
+var autoPaused = false;
 
-// Click the node.
-click = node => node.firstChild.click();
+/*
+ * Try matching a node for the "skip" or "play" button.
+ * Always click "skip", click "play" if skip has been clicked, causing Netflix to pause.
+ */
+matchAndClick = node => {
+  if (node.nodeType === 1) {
+    if (node.matches('.svg-icon-nfplayerPlay') && autoPaused) {
+      node.parentNode.click();
+      autoPaused = false;
+    }
+    if (node.matches('.skip-credits')) {
+      node.firstChild.click();
+      autoPaused = true;
+    }
+  }
+};
 
 // Observe the DOM for changes.
 obs.observe(document.documentElement, { childList: true, subtree: true });
